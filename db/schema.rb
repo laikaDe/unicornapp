4aa80136f777c2b7dba86361ae18c86c3881317c
Laika Deunert <laikadeunert@MacBook-Air-2.local>
2021-11-11 13:40:51 -0800

    Initiated bycrypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_215305) do
+ActiveRecord::Schema.define(version: 2021_11_11_213732) do
 
   create_table "superpowers", force: :cascade do |t|
     t.string "type"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2021_11_09_215305) do
     t.string "color"
     t.string "personality"
     t.string "quirks"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
