b916914e8a18397272798278a92202a53eb0382c
Laika Deunert <laikadeunert@MacBook-Air-2.local>
2021-11-11 13:14:06 -0800

    1.Added partials for superpower's _form & _superpower 2.Added "    self.inheritance_column = :_type_disabled " in superpower's model to bypass the "type" keyword which is a reserved one in rails

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,12 +1,9 @@
 class SuperpowersController < ApplicationController
 
     def create
-        @unicorn = Unicorn.find(params[:unicorn_id])
-        @superpower = @unicorn.superpowers.create(params[:superpower]).permit(:type, :superpower))
-        redirect_to unicorn_path(@unicorn)
-    end
-
-    def destroy
-    end
+		@unicorn = Unicorn.find(params[:unicorn_id])
+	 	@superpower = @unicorn.superpowers.create(params[:superpower].permit(:type, :description))
+		redirect_to unicorn_path(@unicorn)	
+	end
 
 end
--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -1,3 +1,4 @@
 class Superpower < ApplicationRecord
     belongs_to :unicorn
+    self.inheritance_column = :_type_disabled
 end
