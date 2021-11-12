92969ab5678a45da4901212444c4359dbb9dc9e3
Laika Deunert <laikadeunert@MacBook-Air-2.local>
2021-11-11 16:36:31 -0800

    Added 3 new gems for plan to build omniath authenication

--- Gemfile
+++ Gemfile
@@ -22,6 +22,13 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+#Flexible authentication solution for Rails with Warden
+gem 'devise', '~> 4.8'
+
+gem 'omniauth'
+gem 'dotenv-rails'
+gem 'omniauth-google-oauth2'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,29 @@
 class UsersController < ApplicationController
+    skip_before_action :verified_user, only: [:new, :create]
 
+  #get request going to render out the user form
     def new
         @user = User.new
     end
 
-    def create 
-        @user = User.new(user_params)
-        if @user.save
-            flash[:notice] = "User created."
-            redirect_to root_path
-        else
-            render 'new'
-        end
+    def create
+        @user = User.find_by(username:params[:username])
+        if !@user
+            @error = "I'm sorry, that username is incorrect"
+            render :new
+        elsif !user.authenticate(params[:password])
+            @error =  "Password was Incorrect"
+            render :new  
+        else 
+            session[:user_id] = @user.id
+            redirect_to unicorns_path 
+        end 
     end
+  end 
 
-    private
 
-    def user_params
-        params.require(:user).permit(:username, :password)
-    end
+  private 
+  def user_params
+    params.require(:user).permit(:username,:password)
+  end 
 end
