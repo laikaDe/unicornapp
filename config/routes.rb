10e378b2ebaa9f06c77b8b0a19223271d737a42f
Laika Deunert <laikadeunert@MacBook-Air-2.attlocal.net>
2021-11-16 14:21:32 -0800

    1. Configured routes.rb * Admin controller to set up third party login through google

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
     end
   end
 
-  root to: 'static#home'  
+  root to: 'static#home' 
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
 
   # match '/auth/:google_oauth2/callback'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_220530) do
+ActiveRecord::Schema.define(version: 2021_11_16_221544) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
 
   create_table "superpowers", force: :cascade do |t|
     t.string "type"
