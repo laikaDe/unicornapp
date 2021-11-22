be885761b8d773845bac0ae726842e979b072366
Laika Deunert <laikadeunert@MacBook-Air.local>
2021-11-21 17:44:22 -0800

    Added #new method to SuperpowersController

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -3,15 +3,17 @@ class SuperpowersController < ApplicationController
     def create
 		@unicorn = Unicorn.find(params[:unicorn_id])
 	 	@superpower = @unicorn.superpowers.create(params[:superpower].permit(:type, :description))
-		redirect_to unicorn_path(@unicorn)	
+		redirect_to user_unicorn_path(current_user.id, @unicorn)	
 	end
-	
+
+	def new
+		@unicorn = Unicorn.find(params[:unicorn_id])
+	end
+
     def destroy
 		@unicorn = Unicorn.find(params[:unicorn_id])
 		@superpower = @unicorn.superpowers.find(params[:id])
 		@superpower.destroy
-		redirect_to unicorn_path(@unicorn)
+		redirect_to user_unicorn_path(current_user.id, @unicorn)
 	end
-
-
 end
